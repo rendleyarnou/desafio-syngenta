7bb9734309cfc62237b0236949418b8a095c64ee
Rendley Arnou <rendleyarnou7@gmail.com>
2021-04-18 12:16:27 -0300

    Show pixels implementation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # desafio-syngenta
+
 Desafio para seleção de estágio na Syngenta.
+
+## Objetivos
+
+- Contar a quantidade de pixels verdes na imagem 'Syngenta.bmp'.
+- Descobrir qual a mensagem escondida na imagem.
+
+## Passos
+
+1. Declarar a variável _imagem_ do tipo **BufferedImage** para que fosse possível manipular o arquivo utilizando a linguagem de programação.
+
+2. Carregar o arquivo **.bmp** utilizando **ImageIO** e o método _read_ instanciando um novo arquivo de **File** convertendo a _String_ "Syngenta.bmp" em um nome de caminho abstrato.
+
+3. Declarar as variáveis _height_ e _width_ e atribuir à elas os valores retornados dos métodos _getHeight_ e _getWidth_ da subclasse **BufferedImage**.
+
+4. Declarar as variáveis _qtdGreen_, _pixelValue_, _red_, _redOutput_
+
+**BufferedImage** - Esta subclasse descreve uma imagem com um buffer acessível de dados de imagem.
+
+- **ImageIO** - Uma classe que contém métodos de conveniência estáticos para localizar ImageReaders e ImageWriters e realizar codificação e decodificação simples.
--- src/main/GreenPixelCount.java
+++ src/main/GreenPixelCount.java
@@ -1,21 +1,5 @@
 package main;
 
-//passos
-
-		// Ler imagem - OK
-			//BufferedImage + ImageIO
-		// Descobrir dimensões - OK
-			// imagem.getWidth();
-			// imagem.getHeight();
-		// Percorrer imagem - OK
-			// Laços altura X largura
-		// Encontrar tons de verde
-			// função que gera tons de verde e converte para inteiro
-		// Comparar os inteiros para saber se é verde
-			// incrementa a quantidade de verdes
-		// exibir o resultado
-
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -24,9 +8,9 @@ import javax.imageio.ImageIO;
 public class GreenPixelCount {
 
 	public static void main(String[] args) {
-		
+
 		BufferedImage imagem = null;
-		
+
 		try {
 			imagem = ImageIO.read(new File("Syngenta.bmp"));
 		} catch (Exception e) {
@@ -35,34 +19,70 @@ public class GreenPixelCount {
 
 		int height = imagem.getHeight();
 		int width = imagem.getWidth();
-		
+
 		int qtdGreen = 0;
 		int pixelValue = 0;
-		int red = 0;
-		int green = 0;
-		int blue = 0;
-		int rgb = 0;
-		
-		for(int y = 0; y < height; y++){
-			for(int x = 0; x < width; x++){
-				pixelValue = imagem.getRGB(x, y);  // pega o valor do pixel altura X largura
-				
-				
+		int red, redOutput = 0;
+		int green, greenOutput = 0;
+		int blue, blueOutput = 0;
+
+		// Conta a quantidade de pixels verdes que existem na imagem
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				pixelValue = imagem.getRGB(x, y); // pega o valor do pixel altura X largura
 				red = (pixelValue >> 16) & 0xFF; // pega o valor do vermelho & converte de inteiro para hexa
 				green = (pixelValue >> 8) & 0xFF; // pega o valor do verde & converte de inteiro para hexa
 				blue = pixelValue & 0xFF; // pega o valor do azul & converte de inteiro para hexa
-				rgb = new Color(red, green, blue).getRGB(); // converte para um RGB inteiro
 
-				System.out.println("#" + red + green + blue);
-				
-				if ((0<red && red<150) && (0<green) && (red<green) && (blue == 0) ) { // Confere se é verde
+				if ((0 < red && red < 150) && (0 < green) && (red < green) && (blue == 0)) { // Confere se é verde
 					qtdGreen += 1;
+					redOutput = (pixelValue >> 16) & 0xFF;
+					greenOutput = (pixelValue >> 8) & 0xFF;
+					blueOutput = pixelValue & 0xFF;
 				}
-				
 			}
 		}
-		System.out.println("qtdGreen: " + qtdGreen);
-		System.out.println("toString: " + imagem);
+
+		System.out.println("Quantidade de pixels verdes: " + qtdGreen);
+		System.out.println();
+		System.out.println("HEX da cor:");
+		System.out.println("| " + "R" + "  |  " + "G" + "  |  "  + "B" + " |" );
+		System.out.println("| " + redOutput + " | " + greenOutput + " |  " + blueOutput + " |");
+//		System.out.println("Valor RGB: " + "R:" + redOutput + " " + "G:" + greenOutput + " " + "B:" + blueOutput);
+
+		
+//		String matriz[][] = new String[420][300];
+//
+////		aloca os valores na matriz de string
+//		for (int y = 0; y < height; y++) {
+//			for (int x = 0; x < width; x++) {
+//				pixelValue = imagem.getRGB(x, y);
+//				red = (pixelValue >> 16) & 0xFF;
+//				green = (pixelValue >> 8) & 0xFF;
+//				blue = pixelValue & 0xFF;
+//
+//				if ((0 < red && red < 150) && (0 < green) && (red < green) && (blue == 0)) {
+//					matriz[x][y] = "°";
+//				} else {
+//					matriz[x][y] = " ";
+//				}
+//			}
+//		}
+//
+////		 imprime a matriz com os valores em hex ou símbolos '#' e '0'
+//		for (int y = 0; y < height; y++) {
+//			for (int x = 0; x < width; x++) {
+////				pixelValue = imagem.getRGB(x, y);
+////				red = (pixelValue >> 16) & 0xFF;
+////				green = (pixelValue >> 8) & 0xFF;
+////				blue = pixelValue & 0xFF;
+////				System.out.print(" #" + red + green + blue + "");
+//
+//				System.out.print(matriz[x][y]);
+//			}
+//			System.out.println();
+//		}
+
 	}
-	
+
 }
